47ff93339d6213ea966dc1740e4cdd0228712c23
Origin-Core <vidox.iran@gmail.com>
2026-08-02 18:32:15 +0330

    Update OriginVpnApplication.kt

--- app/src/main/java/com/origin/vpn/OriginVpnApplication.kt
+++ app/src/main/java/com/origin/vpn/OriginVpnApplication.kt
@@ -1,6 +1,7 @@
 package com.origin.vpn
 
 import android.app.Application
+import com.origin.vpn.utils.AppContextProvider
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -8,6 +9,10 @@ import timber.log.Timber
 class OriginVpnApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        
+        // Initialize AppContextProvider
+        AppContextProvider.init(this)
+        
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
